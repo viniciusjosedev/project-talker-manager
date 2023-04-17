2667d33fc2b8736d6b133f3bee5efdaecff78024
Vinicius José <viniciusjosedev@gmail.com>
2023-04-17 17:35:43 -0300

    inital commit :fire:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,11 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+	const data = JSON.parse( await fs.readFile(path.resolve(__dirname, 'talker.json')));
+  response.status(200).json(data);
 });
 
 app.listen(PORT, () => {
