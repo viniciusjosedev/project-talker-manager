09e8ff01a06a0e61cc66599568e1207ff85c7798
Vinicius José <viniciusjosedev@gmail.com>
2023-04-17 19:59:06 -0300

    crate: requirement 1 and 2 done

--- src/index.js
+++ src/index.js
@@ -14,8 +14,16 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_request, response) => {
-	const data = JSON.parse( await fs.readFile(path.resolve(__dirname, 'talker.json')));
+  const data = JSON.parse(await fs.readFile(path.resolve(__dirname, 'talker.json')));
   response.status(200).json(data);
+});
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const data = JSON.parse(await fs.readFile(path.resolve(__dirname, 'talker.json')));
+  const filter = data.find((e) => e.id === Number(id));
+  if (!filter) return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return response.status(200).json(filter);
 });
 
 app.listen(PORT, () => {
